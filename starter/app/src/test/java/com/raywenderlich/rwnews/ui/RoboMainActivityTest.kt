9b5f761678d6d031af3c5f260aec4c6f572926db
chuy-zip <andreschuy03@gmail.com>
2023-10-11 17:23:11 -0600

    Finished part 1 of tutorial Robolectric

--- starter/app/src/test/java/com/raywenderlich/rwnews/ui/RoboMainActivityTest.kt
+++ starter/app/src/test/java/com/raywenderlich/rwnews/ui/RoboMainActivityTest.kt
@@ -9,23 +9,55 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.raywenderlich.rwnews.di.ActivityModule
+import com.raywenderlich.rwnews.ui.MainActivity
+import com.raywenderlich.rwnews.ui.navigation.NavigationHelper
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.UninstallModules
+import com.google.common.truth.Truth.assertThat
+import com.raywenderlich.rwnews.ui.list.NewsListFragment
+import com.raywenderlich.rwnews.R
 
-@HiltAndroidTest // 1
-@Config(application = HiltTestApplication::class) // 2
-@RunWith(RobolectricTestRunner::class) // 3
-@LooperMode(LooperMode.Mode.PAUSED)  // 4
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
+@RunWith(RobolectricTestRunner::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+@UninstallModules(ActivityModule::class) // HERE
 class RoboMainActivityTest {
 
-    @get:Rule
-    var hiltAndroidRule = HiltAndroidRule(this) // 5
+    @BindValue // 1
+    @JvmField // 2
+    val navigator: NavigationHelper = FakeNavigationHelper()  // 3
+
+    @get:Rule(order = 0)
+    var hiltAndroidRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1) // 2
+    var activityScenarioRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java) // 1
 
     @Before
     fun setUp() {
-        hiltAndroidRule.inject() // 6
+        hiltAndroidRule.inject()
     }
 
     @Test
-    fun whenMainActivityLaunchedNavigationHelperIsInvokedForFragment() { // 7
-        assertTrue(true)
+    fun whenMainActivityLaunchedNavigatorIsInvokedForFragment() {
+        activityScenarioRule.scenario // 3
+    }
+
+    @Test
+    fun whenMainActivityLaunchedNavigationHelperIsInvokedForFragment() {
+        activityScenarioRule.scenario // 1
+        val fakeHelper = navigator as FakeNavigationHelper // 2
+        with(fakeHelper.replaceRequests[0]) { // 3
+            assertThat(anchorId)
+                .isEqualTo(R.id.anchor)
+            assertThat(fragment)
+                .isInstanceOf(NewsListFragment::class.java)
+            assertThat(backStack)
+                .isNull()
+        }
     }
 }
